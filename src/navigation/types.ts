7174fe2b778d09494cb21b8865b8cc49872249b2
Flavio Gouveia <flaviorgouveia@gmail.com>
2025-05-14 23:11:43 +0100

    Fix search screen

---
+++ src/navigation/types.ts
@@ -0,0 +1,10 @@
+export type RootStackParamList = {
+  Home: undefined;
+  Details: {
+    movie: string;
+    title: string;
+    description: string;
+    headerImage: string;
+  };
+  // Add other screens as needed
+};
